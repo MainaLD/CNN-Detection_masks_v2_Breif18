1d97088939e4b0455be452b7c5c01f78048baa69
MainaLD <maina.ledem@gmail.com>
2022-05-11 15:03:44 +0200

    avec model.hdf5 dans le github

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from PIL import Image
 import cv2
 from detect_mask import afficher_visage
 
+import tensorflow as tf
+from keras.layers import Dense, Flatten
+from keras.models import Model, Sequential
+from tensorflow import keras
+filepath_model = 'model.hdf5'
+
 image_defaut = "./images/004.jpg"
 
 @st.cache
@@ -14,8 +20,19 @@ def load_image(img):
 def convert_df(df):
     return df.to_csv().encode('utf-8')
 
-def detecter_mask(image):
-    visages, list_nom_img, tableau = afficher_visage(image)
+def import_model(filepath_model):
+    # pour .hdf5
+    model = tf.keras.applications.MobileNet(include_top=False,weights="imagenet",input_shape=(224, 224, 3))
+    flat1 = Flatten()(model.layers[-1].output)
+    class1 = Dense(256, activation='relu')(flat1)
+    output = Dense(2, activation='softmax')(class1)
+    model = Model(inputs = model.inputs, outputs = output)
+    model.load_weights(filepath_model)
+    return model
+
+
+def detecter_mask(image, model):
+    visages, list_nom_img, tableau = afficher_visage(image, model)
 
     st.image(visages, use_column_width='auto', caption= list_nom_img, width=10)
     for idx,name in enumerate(tableau['Masque'].value_counts().index.tolist()):
@@ -33,7 +50,8 @@ def main():
     
     activities = ["Importer image","Image par webcam","Autre"]
     choice = st.sidebar.selectbox("Choix",activities)
-    
+    model = import_model(filepath_model)
+
     if choice == 'Importer image':
         st.subheader("Détection de masques sur 1 image")      
         image_file = st.file_uploader("Charger une image",type=['jpg','png','jpeg'])
@@ -41,14 +59,14 @@ def main():
         if image_file is not None:    
             st.image(image_file, use_column_width='auto', caption= "Image originale")
             if st.button("Détecter les masques"):
-                detecter_mask(image_file)
+                detecter_mask(image_file, model)
         
         else:
             img_defaut = Image.open(image_defaut)
             st.text("Image chargée par défaut")
             st.image(img_defaut, use_column_width='auto')
             if st.button("Détecter les masques"):
-                detecter_mask(image_defaut)
+                detecter_mask(image_defaut, model)
 
 
     elif choice == 'Image par webcam':
@@ -56,7 +74,7 @@ def main():
         picture = st.camera_input("Prendre une capture")
 
         if st.button("Détecter les masques"):
-            detecter_mask(picture)
+            detecter_mask(picture, model)
 
 
     elif choice == 'Autre':
--- detect_mask.py
+++ detect_mask.py
@@ -7,22 +7,38 @@ import keras
 import tensorflow as tf
 from tensorflow import keras
 
-filepath = './model_mask.h5'
+# import tensorflow as tf
+# from keras.layers import Dense, Flatten
+# from keras.models import Model, Sequential
+# from tensorflow import keras
+# filepath_model = './model.hdf5'
 # loaded_model = keras.models.load_model(filepath)
 
-liste_label = ['sans masque', 'avec masque']
+liste_label = ['avec masque', 'sans masque']
 # Modele de détection des visage
 cascade_path = "./cascades/haarcascade_frontalface_default.xml"
 
-def detecter_masks(imgr):
+# def import_model(filepath_model):
+#     # pour .hdf5
+#     model = tf.keras.applications.MobileNet(include_top=False,weights="imagenet",input_shape=(224, 224, 3))
+#     flat1 = Flatten()(model.layers[-1].output)
+#     class1 = Dense(256, activation='relu')(flat1)
+#     output = Dense(2, activation='softmax')(class1)
+#     model = Model(inputs = model.inputs, outputs = output)
+#     model.load_weights(filepath_model)
+#     return model
+
+
+def detecter_masks(imgr, model):
     img_exp = np.expand_dims(np.array(imgr)/255.0, axis=0)
-    # prediction = loaded_model.predict(img_exp)
-    prediction = 1
+    # loaded_model = import_model(filepath_model)
+    prediction = model.predict(img_exp)
+    # prediction = 1
     resultat = liste_label[np.argmax(prediction)]
     image_pred = cv2.putText(imgr, resultat, (20, 30), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (255,0,0))
     return image_pred, resultat
 
-def afficher_visage(img_path):
+def afficher_visage(img_path, model):
     to_image = Image.open(img_path)
     src = cv2.cvtColor(np.array(to_image), cv2.COLOR_RGB2BGR)
     cascade = cv2.CascadeClassifier(cascade_path)
@@ -37,7 +53,7 @@ def afficher_visage(img_path):
             imgr = cv2.resize(img_couper,(224,224), interpolation = cv2.INTER_AREA)
             nom_img = "visage{}".format(i+1)
 
-            imgr, mask_port = detecter_masks(imgr)
+            imgr, mask_port = detecter_masks(imgr, model)
             visages.append(cv2.cvtColor(imgr, cv2.COLOR_BGR2RGB))
             list_nom_img.append(nom_img)
 
